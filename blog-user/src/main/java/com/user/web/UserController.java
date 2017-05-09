8c40dfa0a19f547e1e5ab8d88fca214110e1fcb6
codewang <l>
2017-05-09 17:23:56 +0800

    查询用户

--- blog-user/src/main/java/com/user/web/UserController.java
+++ blog-user/src/main/java/com/user/web/UserController.java
@@ -1,8 +1,10 @@
 package com.user.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.user.domain.User;
@@ -22,5 +24,13 @@ public class UserController {
 	public User reg(@RequestBody User user){
 		user=userRepository.save(user);
 		return user;
+	}
+	@GetMapping(value="/get")
+	public User login(@RequestParam Long id){
+		User u=userRepository.getUserById(id);
+		if(u!=null){
+			System.out.println("查询成功。。。。。。。。。"+u.getUserName());
+		}
+		return u;
 	}
 }
