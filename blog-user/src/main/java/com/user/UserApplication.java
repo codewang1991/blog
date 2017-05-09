18952b949b31dfe6fe48948d51fa0b1e7e8fc54e
CodeManWangMin <l>
2017-05-09 14:26:42 +0800

    用户服务增加： 1、启动方法服务端注解； 2、配置文件增加服务器端口和eureka配置

--- blog-user/src/main/java/com/user/UserApplication.java
+++ blog-user/src/main/java/com/user/UserApplication.java
@@ -2,13 +2,14 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 启动类
- * @author wangmin
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
 	
 		public static void main(String[] args) {
