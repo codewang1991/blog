a59f1eea0441e5bef37e513e7a29b4e65dba0a7b
codewang <l>
2017-05-25 16:37:20 +0800

    映射delete请求

--- blog-user/src/main/java/com/user/web/UserController.java
+++ blog-user/src/main/java/com/user/web/UserController.java
@@ -3,6 +3,7 @@ import java.util.Date;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,7 +48,7 @@ public class UserController {
 			@RequestParam(name="pageSize",defaultValue="10",required=false)Integer pageSize){
 		return userService.getUserByName(userName, currentPage, pageSize);
 	}
-	@GetMapping(value="/del/user")
+	@DeleteMapping(value="/del/user")
 	public void delUser(@RequestParam(name="id")String id){
 		userService.delUser(id);
 	}
