3acd0e31cb9fa6b90aaaa813653cc0af706d1f3a
codewang <l>
2017-05-25 11:49:13 +0800

    重构usercontroller代码

--- blog-user/src/main/java/com/user/web/UserController.java
+++ blog-user/src/main/java/com/user/web/UserController.java
@@ -1,6 +1,7 @@
 package com.user.web;
 import java.util.Date;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.user.dao.UserRepository;
 import com.user.domain.MyPage;
 import com.user.domain.User;
+import com.user.service.UserService;
 @RestController
 public class UserController {
 	@Autowired
-	private UserRepository userRepository;
+	private UserService userService;
 	
+	private Logger log=Logger.getLogger(UserController.class);
 	/**
 	 * 注册用户
 	 * @param user
@@ -29,26 +32,24 @@ public class UserController {
 	@PostMapping(value="/add/user")
 	public User reg(@RequestBody User user){
 		user.setCreateTime(new Date());
-		user=userRepository.save(user);
+		user=userService.regUser(user);
 		return user;
 	}
 	@GetMapping(value="/get/user/{id}")
-	public User login(@PathVariable String id){
-		User u=userRepository.getUserById(id);
+	public User getUser(@PathVariable String id){
+		User u=userService.getUserById(id);
 		return u;
 	}
 	@GetMapping(value="/get/users")
 	public MyPage userList(@RequestParam(name="currentPage",defaultValue="1",required=false)Integer currentPage,
 			@RequestParam(name="pageSize",defaultValue="10",required=false)Integer pageSize){
-		Page<User> page=userRepository.findAll(new PageRequest(currentPage-1, pageSize,new Sort(Direction.DESC, "createTime")));
 		
-		return new MyPage(page.getContent(),page.getTotalPages(),page.getTotalElements(),page.getSize(),currentPage);
+		return userService.getAllUserByPage(currentPage, pageSize);
 	}
 	@GetMapping(value="/get/user")
 	public MyPage queryByUserName(@RequestParam(name="name") String userName,
 			@RequestParam(name="currentPage",defaultValue="1",required=false)Integer currentPage,
 			@RequestParam(name="pageSize",defaultValue="10",required=false)Integer pageSize){
-		Page<User> page=userRepository.findUserByUserName(userName,new PageRequest(currentPage-1, pageSize,new Sort(Direction.DESC, "createTime")));
-		return new MyPage(page.getContent(),page.getTotalPages(),page.getTotalElements(),page.getSize(),currentPage);
+		return userService.getUserByName(userName, currentPage, pageSize);
 	}
 }
