b01edb443600a522e1859af732cf6ba61de4431d
codewang <l>
2017-05-25 11:38:00 +0800

    增加用户业务层

---
+++ blog-user/src/main/java/com/user/service/UserService.java
@@ -0,0 +1,14 @@
+package com.user.service;
+
+import org.springframework.stereotype.Repository;
+
+import com.user.domain.MyPage;
+import com.user.domain.User;
+@Repository
+public interface UserService {
+
+	public User regUser(User user);
+	public User getUserById(String userId);
+	public MyPage getAllUserByPage(Integer currentPage,Integer pageSize);
+	public MyPage getUserByName(String userName,Integer currentPage,Integer pageSize);
+}
---
+++ blog-user/src/main/java/com/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.user.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.user.dao.UserRepository;
+import com.user.domain.MyPage;
+import com.user.domain.User;
+import com.user.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+	@Autowired
+	private UserRepository userRepository;
+    
+	@Transactional
+	public User regUser(User user) {
+		try{
+			user=userRepository.save(user);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return user;
+	}
+
+	@Override
+	public User getUserById(String userId) {
+		User user=null;
+		try {
+			user=userRepository.getUserById(userId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return user;
+	}
+
+	@Override
+	public MyPage getAllUserByPage(Integer currentPage, Integer pageSize) {
+		 MyPage myPage=null;
+		try{
+		  Page<User> page=userRepository.findAll(new PageRequest(currentPage-1, pageSize,new Sort(Direction.DESC, "createTime")));
+		  myPage=new MyPage(page.getContent(),page.getTotalPages(),page.getTotalElements(),page.getSize(),currentPage);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return myPage;
+	}
+
+	@Override
+	public MyPage getUserByName(String userName, Integer currentPage, Integer pageSize) {
+		 MyPage myPage=null;
+		 try{
+			Page<User> page=userRepository.findUserByUserName(userName,new PageRequest(currentPage-1, pageSize,new Sort(Direction.DESC, "createTime")));
+			myPage=new MyPage(page.getContent(),page.getTotalPages(),page.getTotalElements(),page.getSize(),currentPage);
+		 }catch(Exception e){
+			 e.printStackTrace();
+		 }
+		return myPage;
+	}
+
+	
+
+}
